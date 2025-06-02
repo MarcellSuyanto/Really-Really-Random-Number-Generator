da3f12c6af3256eb4b6e63f6503b531e3c5a7cb5
Marcell Suyanto <marscell@connect.hku.hk>
2025-06-03 03:09:38 +0800

    Added generators

--- bsoup_scraping.py
+++ bsoup_scraping.py
@@ -28,14 +28,45 @@ def get_bbc():
     index = (get_leetcode()//get_wiki())%len(headlines)
     return len(headlines[index].text.strip())
 
-def get_espn():
+def get_astro():
+    url = "https://apod.nasa.gov/apod/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    explanation = soup.get_text().replace("\n", "").replace(",","")
+    explanation = explanation.split(' ')
+    i = explanation.index("Explanation:")
+    j = explanation.index("Piece")
+    explanation = explanation[i:j]
+
+    total = 0 
+    for word in explanation:
+        try:
+            total += int(word)
+        except ValueError:
+            continue
+    if not total:
+        return len(explanation)
+    
+    return total
+    
+
+
+def get_espn(): # in progress, do not use for now
     week = 19
     year = 2022
 
-    url = "https://www.espn.com/nfl/scoreboard/_/week/{week}/year/{year}"
-    response = requests.get(url)
+    url = f"https://www.espn.com/nfl/scoreboard/_/week/{week}/year/{year}"
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com/",
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response)
     soup = BeautifulSoup(response.content, 'html.parser')
-    
+    print(soup.select('.ScoreboardScoreCell'))
     all_data = []
     for board in soup.select('.ScoreboardScoreCell'):
         title = board.find_previous(class_='Card__Header__Title').text
@@ -46,5 +77,5 @@ def get_espn():
 
     print(all_data)
 
-get_espn()
+
 
